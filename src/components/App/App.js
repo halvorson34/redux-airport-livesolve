d105c10dd8c6032763ae6255ff02545a59527b4b
Matt Halvorson <halvorson34.mh@gmail.com>
2020-04-15 18:15:36 -0500

    final commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class App extends Component {
+  state = {
+    airline: "",
+  };
+
+  handleChangeEvent = (event) => {
+    this.setState({
+      airline: event.target.value,
+    });
+  };
+
+  handleSubmitEvent = () => {
+    this.props.dispatch({ type: "ADD_AIRLINE", payload: this.state.airline });
+    this.setState({
+      airline: "",
+    });
+  };
+
   render() {
+    console.log(this.props.store.airlineReducer);
+
+    const airlineArray = this.props.store.airlineReducer.map((item, index) => {
+      return (
+        <tr key={index}>
+          <td>{item}</td>
+        </tr>
+      );
+    });
+
     return (
       <div>
         <h1>Redux Airport</h1>
-        <input placeholder="Airline Name" />
-        <button>Add Airline</button>
+        <input
+          onChange={this.handleChangeEvent}
+          value={this.state.airline}
+          placeholder="Airline Name"
+        />
+        <button onClick={this.handleSubmitEvent}>Add Airline</button>
         <table>
-          {/* Airlines should be listed here */}
+          <thead>
+            <tr>
+              <th>AIRLINE</th>
+            </tr>
+          </thead>
+          <tbody>{airlineArray}</tbody>
         </table>
       </div>
     );
   }
 }
 
-export default App;
+const mapStoreToProps = (store) => ({ store });
+
+export default connect(mapStoreToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers, bindActionCreators } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
 
+const airlineReducer = (state = [], action) => {
+  if (action.type === "ADD_AIRLINE") {
+    return [...state, action.payload];
+  }
 
+  return state;
+};
 
-const airlineReducer = (state = [], airline) => {
-    if (ation.type === "ADD_AIRLINE"){
-        return [...state, action.payload];
-    }
+const storeInstance = createStore(combineReducers({ airlineReducer }));
 
-
-    return state;
-}
-
-const storeInstance = createStore{combineReducers({airlineReducer})};
-
-ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
