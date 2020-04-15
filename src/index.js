2b08a6166a676bf2ec0a9a4e2c8a163c175b8416
Matt Halvorson <halvorson34.mh@gmail.com>
2020-04-15 16:14:48 -0500

    set up reducers in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, bindActionCreators } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const airlineReducer = (state = [], airline) => {
+    if (ation.type === "ADD_AIRLINE"){
+        return [...state, action.payload];
+    }
+
+
+    return state;
+}
+
+const storeInstance = createStore{combineReducers({airlineReducer})};
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
